812974ad589261bd0cf2763f379be1bdb9376ba6
Juliana Hernandez <juliana88h@gmail.com>
2020-09-15 23:50:33 -0700

    added group project files

---
+++ assets/config/config.js
@@ -0,0 +1,17 @@
+var config = {
+  WEATHER_API: "aa2ec539f87be152770b7377dac0c5b9",
+  BING_MAPS_API:
+    "AuWKs9MGdj79ajbAX3YvfI7G0s7E_KyFRUQYmnT3IqBYCANGUF69Pg7Ik32ONfZg",
+  HIKING_API: "200767202-2279739a94fd80423c86d0d03fc4ae16",
+  MAPGL_API:
+    "pk.eyJ1IjoiYmxhY2tmb2dnIiwiYSI6ImNrYWd5MmZncjBkankycHBqeGJtbjh5MXYifQ.YfvFQFSQNRNrDAnVxwFX6Q",
+  firebaseConfig: {
+    apiKey: "AIzaSyAe2rjoAOYzCX27GQxCFdbsl4cqT2vO0xE",
+    authDomain: "my-park-finder.firebaseapp.com",
+    databaseURL: "https://my-park-finder.firebaseio.com",
+    projectId: "my-park-finder",
+    storageBucket: "my-park-finder.appspot.com",
+    messagingSenderId: "317772869860",
+    appId: "1:317772869860:web:8e1f1fe0da05d85aec3cf1",
+  },
+};
